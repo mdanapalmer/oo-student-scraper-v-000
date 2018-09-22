37945fb01b7108dd0be6005700b1fb344b7662c6
Dana Palmer <roiai4@hotmail.com>
2018-09-22 17:26:02 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,19 +1,32 @@
 require 'open-uri'
-
+require'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    #why aren't we using the url here?
     index_page = Nokogiri::HTML(open(index_url))
+    #converts the open-uri into Nokogiri's ability to parse the html as nested arrays and names that var index_page
     students = []
+    #creates an empty student array
     index_page.css("div.roster-cards-container").each do |card|
+      binding.pry
+      #calls the .css method on each element in the html container of index_page
+      #iterates over them with each element referenced as card
       card.css(".student-card a").each do |student|
+        #also calls the.css notation and iterates on the nested array of card into student
+        #so that we can now access our student attributes
         student_profile_link = "#{student.attr('href')}"
+        #interpolates the href into the var
         student_location = student.css('.student-location').text
+        #digs into the student.css array to extract the location text and name it student_location
         student_name = student.css('.student-name').text
+        #obtains student_name text from the .css array student
         students << {name: student_name, location: student_location, profile_url: student_profile_link}
+        #shovels the hash for name, location, and profile_url into the students array
       end
     end
     students
+    #returns the students array
   end
 
   def self.scrape_profile_page(profile_slug)
